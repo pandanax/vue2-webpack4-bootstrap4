1dd2937b6924e2f88d6b13361762ef2ef9910708
Anatoliy Shishkin <anatoliy.shishkin@wikimart.ru>
2019-06-06 00:11:27 +0300

    webpack npm run build is done

--- build/utils.js
+++ build/utils.js
@@ -45,10 +45,7 @@ exports.cssLoaders = function (options) {
     // Extract CSS when that option is specified
     // (which is the case during production build)
     if (options.extract) {
-      return ExtractTextPlugin.extract({
-        use: loaders,
-        fallback: 'vue-style-loader'
-      })
+      return [ExtractTextPlugin.loader].concat(loaders)
     } else {
       return ['vue-style-loader'].concat(loaders)
     }
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,6 +10,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 
 const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -37,7 +39,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new UglifyJsPlugin({
       uglifyOptions: {
         compress: {
-          warnings: false
+          //warnings: false
         }
       },
       sourceMap: config.build.productionSourceMap,
@@ -83,7 +85,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // enable scope hoisting
     new webpack.optimize.ModuleConcatenationPlugin(),
     // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
+    /*new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       minChunks (module) {
         // any required modules inside node_modules are extracted to vendor
@@ -95,22 +97,22 @@ const webpackConfig = merge(baseWebpackConfig, {
           ) === 0
         )
       }
-    }),
+    }),*/
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
+    /*new webpack.optimize.CommonsChunkPlugin({
       name: 'manifest',
       minChunks: Infinity
-    }),
+    }),*/
     // This instance extracts shared chunks from code splitted chunks and bundles them
     // in a separate chunk, similar to the vendor chunk
     // see: https://webpack.js.org/plugins/commons-chunk-plugin/#extra-async-commons-chunk
-    new webpack.optimize.CommonsChunkPlugin({
+    /*new webpack.optimize.CommonsChunkPlugin({
       name: 'app',
       async: 'vendor-async',
       children: true,
       minChunks: 3
-    }),
+    }),*/
 
     // copy custom static assets
     new CopyWebpackPlugin([
@@ -145,5 +147,31 @@ if (config.build.bundleAnalyzerReport) {
   const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
   webpackConfig.plugins.push(new BundleAnalyzerPlugin())
 }
+
+webpackConfig.plugins.push(
+  new VueLoaderPlugin()
+)
+
+webpackConfig.optimization = Object.assign({},(webpackConfig.optimization)?{}:webpackConfig.optimization, {
+  splitChunks: {
+    cacheGroups: {
+      app: {
+        name: 'app',
+        chunks: 'initial',
+        minChunks: 2
+      },
+      manifest: {
+        name: 'manifest',
+        minChunks: Infinity
+      },
+      vendor: {
+        name: 'vendor',
+        test: /[\\/]node_modules[\\/]/,
+        chunks: 'all'
+      }
+    }
+  }
+})
+
 
 module.exports = webpackConfig
